c035b5f278bcbd43fc15a149772306aba625be8d
Hylanleong <33160717+Hylanleong@users.noreply.github.com>
2017-10-28 02:55:10 +0800

    L&L Espressso Lab

---
+++ Feedback.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Feedback : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Dell\Documents\Visual Studio 2012\WebSites\WebSite14\App_Data\Database.mdf;Integrated Security=True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = System.Data.CommandType.Text;
+        cmd.CommandText= "insert into Feedback values('"+TextBox1.Text+"','"+TextBox2.Text+"','"+TextBox3.Text+"','"+TextBox4.Text+"')";
+        cmd.ExecuteNonQuery();
+
+        con.Close();
+       
+    }
+   
+}
